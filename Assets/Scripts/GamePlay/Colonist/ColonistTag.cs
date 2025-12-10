c6c7b179bcc805997e13db2d5a82bde976f35c13
dylss <dylanbrugman@hotmail.com>
2025-12-11 00:56:06 +0100

    Added ScriptsV2 as start of new setup of scripts

--- Assets/Scripts/GamePlay/Colonist/ColonistTag.cs
+++
@@ -1,8 +0,0 @@
-﻿using Core.ECS;
-
-namespace GamePlay.Colonist {
-    public struct ColonistTag : IComponent {
-        public string ConfigId;
-        public string Name;
-    }
-}
--- Assets/Scripts/GamePlay/Inventory/Stackable.cs
+++
@@ -1,7 +0,0 @@
-﻿using Core.ECS;
-
-namespace GamePlay.Inventory {
-    public class Stackable : IComponent {
-        public byte Count { get; set; }
-    }
-}
